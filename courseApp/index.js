e7407bbd2da72e7e903f549a4e1a8b10081186a3
NIK-llama <nikshitog@gmail.com>
2025-09-12 18:37:08 +0530

    courseApp

--- courseApp/index.js
+++ courseApp/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
+
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
--- courseApp/routes/user.js
+++ courseApp/routes/user.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const { UserModel } = require("../db");
+const jwt = require("jsonwebtoken");
 
 const userRouter = Router();
 
 userRouter.post("/signup", function(req, res) {
+    const requiredBody = z.object({
+        
+    })
+    const email = req.body.email;
+    const password = req.body.password;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
     
 })
 
