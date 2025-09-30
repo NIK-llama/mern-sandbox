4cf23ba86c523aea357d1770400636f7476354c5
NIK-llama <nikshitog@gmail.com>
2025-09-30 23:09:02 +0530

    typescript interfaces

---
+++ typescript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(user) {
+}
+let user = {
+    firstname: "nik",
+    lastname: "c",
+    age: 21
+};
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,15 @@
+interface UserType {
+    firstname: String,
+    lastname: String,
+    age: number
+}
+
+function greet(user: UserType) {
+
+}
+
+let user: UserType = {
+    firstname: "nik",
+    lastname: "c",
+    age: 21
+}
