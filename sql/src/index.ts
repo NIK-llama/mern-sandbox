ef0c6fd7669be95cce23251e9dea1ebd80982f11
NIK-llama <nikshitog@gmail.com>
2025-12-14 22:38:40 +0530

    re - sql

--- sql/src/index.ts
+++ sql/src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "pg";
 
 const client = new Client(
-  "postgresql://neondb_owner:npg_7kFVegYTZKc4@ep-delicate-block-ady23i6o-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  ""
 );
 
 async function main() {
