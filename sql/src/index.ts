c031e81cef4cb31ca2d727cc8a5c2332ace8d00b
NIK-llama <nikshitog@gmail.com>
2025-12-18 17:13:28 +0530

    relationships, joins and transactions

--- sql/src/index.ts
+++ sql/src/index.ts
@@ -1,21 +1,98 @@
 import { Client } from "pg";
 
-async function insertData(username: string, email: string, password: string) {
-  const client = new Client(
-    ""
-  );
+const client = new Client({
+  connectionString:
+    "postgresql://neondb_owner:npg_7kFVegYTZKc4@ep-delicate-block-ady23i6o-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
+async function connectDB() {
+  await client.connect();
+}
+
+async function insertData(
+  username: string,
+  email: string,
+  password: string,
+  city: string,
+  country: string,
+  street: string,
+  pincode: string
+) {
   try {
-    await client.connect();
-    const insertQuery = "INSERT INTO users(username, email, password) VALUES($1, $2, $3)";
-    const values = [username, email, password];
-    const res = await client.query(insertQuery, values);
-    console.log("insertion success:",res);
-  } catch(err) {
-    console.error("error during insertion: ",err);
+    await client.query("BEGIN");
+
+    const insertUserQuery =
+      "INSERT INTO users(username, email, password) VALUES ($1, $2, $3) RETURNING id";
+
+    const userRes = await client.query(insertUserQuery, [
+      username,
+      email,
+      password,
+    ]);
+
+    const userId = userRes.rows[0].id;
+
+    const insertAddressQuery =
+      "INSERT INTO addresses(city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5)";
+
+    await client.query(insertAddressQuery, [
+      city,
+      country,
+      street,
+      pincode,
+      userId,
+    ]);
+
+    await client.query("COMMIT");
+    console.log("Insertion successful");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error("Error during insertion:", err);
+  }
+}
+
+async function getUserDetailsWithAddress(id: number) {
+  try {
+    const query = `
+      SELECT 
+        u.id,
+        u.username,
+        u.email,
+        a.city,
+        a.country,
+        a.street,
+        a.pincode
+      FROM users u
+      JOIN addresses a ON u.id = a.user_id
+      WHERE u.id = $1
+    `;
+
+    const res = await client.query(query, [id]);
+    return res.rows;
+  } catch (err) {
+    console.error("Error fetching user details:", err);
+  }
+}
+
+async function main() {
+  try {
+    await connectDB();
+
+    await insertData(
+      "sik123",
+      "sik321@nik.com",
+      "sik321",
+      "jsss",
+      "jsxx",
+      "jsyy",
+      "34134"
+    );
+
+    const user = await getUserDetailsWithAddress(3);
+    console.log(user);
   } finally {
     await client.end();
   }
 }
 
-insertData("nik321", "nik321@nik.com", "nik321").catch(console.error);
+main().catch(console.error);
